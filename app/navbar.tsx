404a18240d0bbf501fd04ea58125ce026efc0e92
Orban Tamas <orbant1@gmail.com>
2024-04-05 21:45:06 +0200

    first layout update

---
+++ app/navbar.tsx
@@ -0,0 +1,22 @@
+import "./css/navbar.css"
+
+export default function NavBar() {
+
+return(
+    <div className="navbar">
+        <div className="nav-logo">
+            Logo
+        </div>
+        <div className="nav-menu">
+            <h5>Valami</h5>
+            <h5>Other</h5>
+            <h5>Jami</h5>
+            <h5>Bolt</h5>
+        </div>
+        <div className="nav-other">
+            <h5>1</h5>
+            <h5>2</h5>
+        </div>
+    </div>
+
+)}
